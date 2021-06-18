7a1c199096960fa22bc3a415b4350f128a049a8a
Seungyong Han <85474053+Expert-Han@users.noreply.github.com>
2021-06-19 00:49:32 +0900

    Update RungeKutta4th.py

--- RungeKutta4th.py
+++ RungeKutta4th.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+# The system dynamics is given as that of the pdf file.
 def plant(x, u):
     dx[0] = x[1]
     dx[1] = 980 * x[0] - 2.8 * x[2]
